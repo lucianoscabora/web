ebd744377cd28efb0cf60878963c673bb0918e67
Luciano Scabora <lucianoscabora@gmail.com>
2023-01-16 23:34:41 -0300

    frontend finished

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: [
+    './src/**/*.tsx',
+    './index.html'
+  ],
   theme: {
     extend: {},
   },
